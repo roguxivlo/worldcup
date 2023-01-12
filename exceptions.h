cdb12c983d64396faee01fc85155601ec158e667
Jan Roguwski <Jroguwski@gmail.com>
2023-01-13 00:37:06 +0100

    compress exceptions headers

---
+++ exceptions.h
@@ -0,0 +1,11 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <exception>
+
+class TooFewDiceException : public std::exception {};
+class TooManyDiceException : public std::exception {};
+class TooFewPlayersException : public std::exception {};
+class TooManyPlayersException : public std::exception {};
+
+#endif  // EXCEPTIONS_H
