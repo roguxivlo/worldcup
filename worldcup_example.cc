393b5aad9b46eac8e65e0d452f0d19db865911ac
btcaf <c.botta2003@gmail.com>
2023-01-15 20:20:25 +0100

    fixed match bankruptcy bug

--- worldcup_example.cc
+++ worldcup_example.cc
@@ -1,55 +1,82 @@
-// Authors: Antoni Stachowski, Michał Kostyk
-// Date: 2023-01-15
-// Description: Testy dla WorldCup2022
-// License: Do What The Fuck You Want To Public License (WTFPL)
-
-#include <sstream>
-#include <memory>
-#include <string>
 #include <cassert>
-#include "worldcup2022.h"
+#include <memory>
+#include <sstream>
+#include <string>
 #include <iostream>
-#include "scoreboard.h"
-#include "test_dice.h"
-#include "regex"
 
-std::string const RED = "\033[1;31m";
-std::string const GREEN = "\033[1;32m";
-std::string const RESET = "\033[0m";
+#include "worldcup2022.h"
+
+class ExampleDie : public Die {
+public:
+    [[nodiscard]] unsigned short roll() const override {
+        static unsigned int rollNum = 0;
+        unsigned short rolls[] = {1, 1, 1, 2, 1, 3};
+        return rolls[rollNum++ % 6];
+    }
+};
+
+class TextScoreBoard : public ScoreBoard {
+    std::stringstream info;
+
+public:
+    void onRound(unsigned int roundNo) override {
+        info << "=== Runda: " << roundNo << "\n";
+    }
+
+    void onTurn(std::string const &playerName, std::string const &status,
+                std::string const &currentSquareName,
+                unsigned int money) override {
+        info << playerName << " [" << status << "] [" << money << "] - "
+             << currentSquareName << "\n";
+    }
+
+    void onWin(const std::string &playerName) override {
+        info << "=== Zwycięzca: " << playerName << "\n";
+    }
+
+    std::string str() { return info.str(); }
+};
 
 int main() {
-    std::cout << RESET << "Results test 3 running\n" << RESET;
-
-    std::shared_ptr<Die> die1 = std::make_shared<SnakeEyeDie>();
-    std::shared_ptr<Die> die2 = std::make_shared<WeirdlySpecificDie>();
-
-    std::shared_ptr<TextScoreBoard> scoreboard = std::make_shared<TextScoreBoard>();
+    std::shared_ptr<Die> die1 = std::make_shared<ExampleDie>();
+    std::shared_ptr<Die> die2 = std::make_shared<ExampleDie>();
+    std::shared_ptr<TextScoreBoard> scoreboard =
+            std::make_shared<TextScoreBoard>();
 
     std::shared_ptr<WorldCup> worldCup2022 = std::make_shared<WorldCup2022>();
     worldCup2022->addDie(die1);
     worldCup2022->addDie(die2);
-    worldCup2022->addPlayer("Kleofas");
-    worldCup2022->addPlayer("Ildefons");
-    worldCup2022->addPlayer("Godehard");
+    worldCup2022->addPlayer("Lewandowski");
+    worldCup2022->addPlayer("Messi");
+    worldCup2022->addPlayer("Ronaldo");
     worldCup2022->setScoreBoard(scoreboard);
 
-    worldCup2022->play(5);
-
-    const std::regex RoundOne("^=== Runda: 0\nKleofas \\[\\*\\*\\* czekanie: 3 \\*\\*\\*] \\[620] - Żółta kartka\nIldefons \\[w grze] \\[620] - Mecz z Meksykiem\nGodehard \\[w grze] \\[1280] - Mecz z Lie*chtensteinem\n(.*\n)*$");
-    const std::regex RoundTwo("^(.*\n)*=== Runda: 1\nKleofas \\[\\*\\*\\* czekanie: 2 \\*\\*\\*] \\[620] - Żółta kartka\nIldefons \\[w grze] \\[440] - Bukmacher\nGodehard \\[w grze] \\[1680] - Mecz z Arabią Saudyjską\n(.*\n)*$");
-    const std::regex RoundThree("^(.*\n)*=== Runda: 2\nKleofas \\[\\*\\*\\* czekanie: 1 \\*\\*\\*] \\[620] - Żółta kartka\nIldefons \\[w grze] \\[310] - Gol\nGodehard \\[w grze] \\[850] - Rzut karny\n(.*\n)*$");
-    const std::regex RoundFour("^(.*\n)*=== Runda: 3\nKleofas \\[w grze] \\[1290] - Mecz z Argentyną\nIldefons \\[\\*\\*\\* bankrut \\*\\*\\*] \\[0] - Mecz z San Marino\nGodehard \\[w grze] \\[1380] - Mecz z San Marino\n(.*\n)*$");
-    const std::regex RoundFive("^(.*\n)*=== Runda: 4\nKleofas \\[w grze] \\[4130] - Mecz z Francją\nGodehard \\[w grze] \\[480] - Bukmacher\n=== Zwycięzca: Kleofas\\s*$");
-
+    worldCup2022->play(100);
     std::cerr << scoreboard->str();
-    std::cerr << RED;
-    assert (std::regex_match(scoreboard->str(), RoundOne));
-    assert (std::regex_match(scoreboard->str(), RoundTwo));
-    assert (std::regex_match(scoreboard->str(), RoundThree));
-    assert (std::regex_match(scoreboard->str(), RoundFour));
-    assert (std::regex_match(scoreboard->str(), RoundFive));
-
-    std::cout << GREEN << "Results test 3 passed\n\n" << RESET;
+    assert(scoreboard->str() ==
+           "=== Runda: 0\n"
+           "Lewandowski [w grze] [840] - Dzień wolny od treningu\n"
+           "Messi [w grze] [840] - Mecz z Liechtensteinem\n"
+           "Ronaldo [*** czekanie: 3 ***] [620] - Żółta kartka\n"
+           "=== Runda: 1\n"
+           "Lewandowski [*** czekanie: 3 ***] [620] - Żółta kartka\n"
+           "Messi [w grze] [540] - Mecz z Arabią Saudyjską\n"
+           "Ronaldo [*** czekanie: 2 ***] [620] - Żółta kartka\n"
+           "=== Runda: 2\n"
+           "Lewandowski [*** czekanie: 2 ***] [620] - Żółta kartka\n"
+           "Messi [w grze] [290] - Mecz z Francją\n"
+           "Ronaldo [*** czekanie: 1 ***] [620] - Żółta kartka\n"
+           "=== Runda: 3\n"
+           "Lewandowski [*** czekanie: 1 ***] [620] - Żółta kartka\n"
+           "Messi [w grze] [340] - Początek sezonu\n"
+           "Ronaldo [w grze] [140] - Bukmacher\n"
+           "=== Runda: 4\n"
+           "Lewandowski [w grze] [665] - Mecz z Argentyną\n"
+           "Messi [w grze] [180] - Dzień wolny od treningu\n"
+           "Ronaldo [*** bankrut ***] [0] - Mecz z Francją\n"
+           "=== Runda: 5\n"
+           "Lewandowski [w grze] [315] - Początek sezonu\n"
+           "Messi [*** bankrut ***] [0] - Żółta kartka\n"
+           "=== Zwycięzca: Lewandowski\n");
     return 0;
-
 }
