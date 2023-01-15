1a19a75be90428697cdb53f6d9a85d2012e4cfb5
Jan Roguwski <Jroguwski@gmail.com>
2023-01-15 22:36:50 +0100

    n_squares

--- board.h
+++ board.h
@@ -2,8 +2,8 @@
 #define BOARD_H
 
 #include <map>
-#include <vector>
 #include <memory>
+#include <vector>
 
 #include "bookie.h"
 #include "goal.h"
@@ -19,26 +19,29 @@ class Player;
 using square_list_t = std::vector<std::shared_ptr<Square>>;
 
 class Board {
-public:
-    std::shared_ptr<Square> get_square(size_t position) { return squares[position]; }
+ public:
+  static const size_t N_SQUARES = 12;
+  std::shared_ptr<Square> get_square(size_t position) const {
+    return squares[position];
+  }
 
-    size_t get_n_squares() const { return N_SQUARES; }
+  size_t get_n_squares() const { return N_SQUARES; }
 
-private:
-    static const size_t N_SQUARES = 12;
-    const square_list_t squares = {
-            std::make_shared<Start>("Początek sezonu", 50),
-            std::make_shared<Match>("Mecz z San Marino", 160, Match::FRIENDLY),
-            std::make_shared<NoTraining>("Dzień wolny od treningu"),
-            std::make_shared<Match>("Mecz z Liechtensteinem", 220, Match::FRIENDLY),
-            std::make_shared<YellowCard>("Żółta kartka", 3),
-            std::make_shared<Match>("Mecz z Meksykiem", 300, Match::GROUP_STAGE),
-            std::make_shared<Match>("Mecz z Arabią Saudyjską", 280, Match::GROUP_STAGE),
-            std::make_shared<Bookie>("Bukmacher", 3, 100, 100),
-            std::make_shared<Match>("Mecz z Argentyną", 250, Match::GROUP_STAGE),
-            std::make_shared<Goal>("Gol", 120),
-            std::make_shared<Match>("Mecz z Francją", 400, Match::FINAL),
-            std::make_shared<Penalty>("Rzut karny", 180)};
+ private:
+  const square_list_t squares = {
+      std::make_shared<Start>("Początek sezonu", 50),
+      std::make_shared<Match>("Mecz z San Marino", 160, Match::FRIENDLY),
+      std::make_shared<NoTraining>("Dzień wolny od treningu"),
+      std::make_shared<Match>("Mecz z Liechtensteinem", 220, Match::FRIENDLY),
+      std::make_shared<YellowCard>("Żółta kartka", 3),
+      std::make_shared<Match>("Mecz z Meksykiem", 300, Match::GROUP_STAGE),
+      std::make_shared<Match>("Mecz z Arabią Saudyjską", 280,
+                              Match::GROUP_STAGE),
+      std::make_shared<Bookie>("Bukmacher", 3, 100, 100),
+      std::make_shared<Match>("Mecz z Argentyną", 250, Match::GROUP_STAGE),
+      std::make_shared<Goal>("Gol", 120),
+      std::make_shared<Match>("Mecz z Francją", 400, Match::FINAL),
+      std::make_shared<Penalty>("Rzut karny", 180)};
 };
 
 #endif  // BOARD_H
--- player.cc
+++ player.cc
@@ -37,7 +37,7 @@ std::shared_ptr<Square> Player::play_round(Dice &dice, Board &board) {
         if (moves > 0) {
             for (size_t i = 0; i < moves - 1; ++i) {
                 ++position;
-                position %= board.get_n_squares();
+                position %= Board::N_SQUARES;
                 if (status != BANKRUPT) {
                     square = board.get_square(position).get();
                     square->passing_action(*this);
